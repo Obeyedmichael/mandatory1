7ba61513134d043d059b5cc4cc1dfef4b84b634e
Obeyedmichael <iamjusthereforfun@hotmail.dk>
2019-03-19 09:43:38 +0100

    exercise 4 50%

---
+++ mandatory1/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using mandatory1.ViewModels;
+
+namespace mandatory1.Controllers
+{
+    public class ContactFormSurfaceController : Umbraco.Web.Mvc.SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+    }
+}
